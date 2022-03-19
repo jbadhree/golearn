0110a8c49be1a9436d1546bb0b068f548de7c27c
Badhree <jbadhree@gmail.com>
2022-03-19 18:13:47 -0400

    Create escapeanalysis.go

---
+++ escapeanalysis.go
@@ -0,0 +1,71 @@
+package main
+
+// Represents a user in the system
+type user struct {
+	name  string
+	email string
+}
+
+func main() {
+	u1 := createUserV1()
+	u2 := createUserV2()
+
+	println("u1", &u1, "u2", &u2)
+
+}
+
+// factory function that creates a user and returns it to the user
+// value semantics
+func createUserV1() user {
+	u := user{
+		name:  "Bill",
+		email: "abc@sbc.com",
+	}
+
+	println("V1", &u)
+
+	return u
+}
+
+// factory function that creates a user and returns it to the user
+// pointer semantics
+func createUserV2() *user {
+	// Clever Code
+	// This will work but return statement will not have &
+	// And it will not tell just by looking at return if the value is in heap or stack
+	/*
+		u := &user{
+			name:  "Bill",
+			email: "abc@sbc.com",
+		}
+	*/
+
+	// Good Code
+	u := user{
+		name:  "Bill",
+		email: "abc@sbc.com",
+	}
+
+	println("V2", &u)
+
+	// Clever Code's return statement
+	// return u
+
+	// Good Code return statement increased readability
+	return &u
+}
+
+/*
+// Bad V2 Function
+func createUserV2() user {
+
+	u := &user{
+		name:  "Bill",
+		email: "abc@sbc.com",
+	}
+
+	println("V2", &u)
+
+	return *u
+}
+*/
